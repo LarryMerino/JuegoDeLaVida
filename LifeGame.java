55f8197b58b272ba4ceabb3ce4d9acb2bc843eef
Larry Merino Zapata <larry@MacBook-Pro-de-Larry.local>
2021-12-14 18:55:42 +0100

    añadida validacion de la extensión del tablero

--- LifeGame.java
+++ LifeGame.java
@@ -4,19 +4,26 @@
  ** de instanciar las clases para arrancar el juego
  */
 
-public class LifeGame{
+public class LifeGame {
 
     private Board board;
     private GameMenu gameMenu;
 
-    public void startGame(){
-        System.out.print("Introduzca la longitud del tablero: ");
-        int size = Integer.parseInt(UtilitiesStrings.readLine());
-        board = new Board(size);
-        board.fillBoard(); 
+    public void startGame() {
+        board = new Board(getBoardExtension());
+        board.fillBoard();
         board.printBoard();
         gameMenu = new GameMenu(board);
         gameMenu.startGameMenu();
     }
 
+    private int getBoardExtension() {
+        while (true) {
+            System.out.print("Introduzca la longitud del tablero: ");
+            String input = UtilitiesStrings.readLine();
+            if (UtilitiesStrings.isValidBoardExtension(input)) {
+                return Integer.parseInt(input);
+            } else System.out.println("Intrduzca un valor mayor que 0");
+        }
+    }
 }
--- UtilitiesStrings.java
+++ UtilitiesStrings.java
@@ -15,4 +15,30 @@ public class UtilitiesStrings {
             throw new RuntimeException(e);
         }
     }
+
+    //Necesita ser un número natural y el cero no está incluido
+    public static boolean isValidBoardExtension(String input) {
+        if (input.isBlank()) return false;
+        String trimInput = input.trim();
+        if (hasCharacters(trimInput) || hasSpacesInBetween(trimInput)) return false;
+        int integerTrimInput = Integer.parseInt(trimInput);
+        if (integerTrimInput < 1) return false;
+        return true; 
+    }
+
+    public static boolean hasCharacters(String trimInput) {
+        char[] trimInputArray = trimInput.toCharArray();
+        for(char c : trimInputArray){
+            if (!Character.isDigit(c)) return true;
+        }
+        return false;
+    }
+
+    public static boolean hasSpacesInBetween(String trimInput) {
+        char[] trimInputArrray = trimInput.toCharArray();
+        for(char c : trimInputArrray){
+            if (Character.isWhitespace(c)) return true;
+        }
+        return false;
+    }
 }
